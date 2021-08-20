4ee0ebd7e2d73b2484e298b84a0dda0d376d6be8
citra anisa <citrapostman12@gmail.com>
2021-08-20 15:29:09 +0700

    Upload file

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SMKN 2 Banjar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final padding = MediaQuery.of(context).viewPadding;
+    print('Applied padding: $padding');
+
+    return SafeArea(
+        child: WebviewScaffold(url: "https://smkn2banjar-jabar.sch.id/"));
+  }
+}
